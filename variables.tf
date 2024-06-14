53e784870e6b6089814ee9e7c373dfc9f422cdb4
Dafik G <g.dafik15@gmail.com>
2024-06-14 12:37:24 +0530

    ec2-module created

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  type = string
+  description = "ami for my-instance"
+}
+
+variable "instance_type" {
+  type = string
+  description = "instance-type of my-instance"
+}
+
+variable "instance_name" {
+  type = string
+  description = "instance-name of my-instance"
+}
